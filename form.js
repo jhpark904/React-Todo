c8e78496b5f74fb24f4b1e9132f2e62bd7e810c4
Jung Hwan Park <jhpark904@gmail.com>
2023-02-14 18:53:50 -0500

    add files

---
+++ form.js
@@ -0,0 +1,25 @@
+// user input - includes validation
+function TodoForm({addTodo}){
+  const [value,setValue] = React.useState('');
+  
+  const handleSubmit = e => {
+    e.preventDefault();
+    if(!value) return;
+    addTodo(value);
+    setValue('');
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input 
+        type="text"
+        className="input"
+        value={value}
+        placeholder="Add Todo..."
+        onChange={e => setValue(e.target.value)} />
+      <button type="submit">
+        Add
+      </button>
+    </form>
+  )
+}
